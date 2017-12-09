2b92340e55decb5340ed6286df591a66250b9ea3
lqw <382394323@qq.com>
2017-12-09 13:26:51 +0800

    Initial commit

---
+++ src/main/java/com/meiya/shiroauthentication/ShiroauthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.meiya.shiroauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShiroauthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShiroauthenticationApplication.class, args);
+	}
+}
---
+++ src/test/java/com/meiya/shiroauthentication/ShiroauthenticationApplicationTests.java
@@ -0,0 +1,16 @@
+package com.meiya.shiroauthentication;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShiroauthenticationApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
